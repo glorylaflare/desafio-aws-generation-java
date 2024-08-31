14a9154e133a113cbbe2406ef7136289a39d6716
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-08-31 20:25:40 -0300

    feat: criando o AlunoRepository

---
+++ src/main/java/org/generation/brazil/grades/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package org.generation.brazil.grades.repository;
+
+import org.generation.brazil.grades.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
