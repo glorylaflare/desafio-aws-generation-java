29e03fe4f87611374df06fb6e28cd782ae304fdd
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-08-31 21:39:20 -0300

    feat: criando o DTO de resposta para um aluno atualizado

---
+++ src/main/java/org/generation/brazil/grades/dto/RespostaAtualizacaoAlunoDTO.java
@@ -0,0 +1,6 @@
+package org.generation.brazil.grades.dto;
+
+import org.generation.brazil.grades.model.DadosCompletosAluno;
+
+public record RespostaAtualizacaoAlunoDTO(String mensagem, DadosCompletosAluno dadosCompletosAluno) {
+}
