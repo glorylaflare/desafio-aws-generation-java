59a8806d836e535f0ed76418f91aa29f3ae786ae
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-09-01 01:21:40 -0300

    fix: remoção do flyway, pois estava dando erro no Docker

--- src/main/resources/db/migration/V1__create_table_alunos.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE alunos (
-    id BIGSERIAL PRIMARY KEY,
-    nome_aluno VARCHAR(255) NOT NULL,
-    idade_aluno INTEGER NOT NULL,
-    nota_primeiro_semestre DECIMAL(4,2) NOT NULL,
-    nota_segundo_semestre DECIMAL(4,2) NOT NULL,
-    nome_professor VARCHAR(255) NOT NULL,
-    numero_da_sala INTEGER NOT NULL
-);
